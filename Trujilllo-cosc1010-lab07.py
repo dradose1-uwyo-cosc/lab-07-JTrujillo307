caa075022cae2c5c0cd1d846e476b488f9e51831
JTrujillo307 <jtruji18@uwyo.edu>
2024-10-30 15:47:59 -0600

    Update and rename cosc1010-lab07.py to Trujilllo-cosc1010-lab07.py

--- Trujilllo-cosc1010-lab07.py
+++ Trujilllo-cosc1010-lab07.py
@@ -1,12 +1,12 @@
-# Your Name Here
+# Jay Trujillo
 # UWYO COSC 1010
 # Submission Date
-# Lab XX
-# Lab Section: 
+# Lab 07
+# Lab Section: 15
 # Sources, people worked with, help given to: 
-# your
-# comments
-# here
+# Kaleb Moler
+# Jhett Carr
+# chat GPT (checked work to see if it was correct)
 
 
 # Prompt the user for an upper bound 
@@ -16,12 +16,28 @@
     # To do so you can use the methods `.isdigit()` or `.isnumeric()`
     # If a user did not enter a number output a statement saying so
 # You will continue to prompt the user until a proper integer value is entered
+def factorial(n):
+    result = 1 
+    while n > 1:
+        result *= n 
+        n -= 1 
+    return result
+        
+def user_number():
+    while True:
+        user_input = input("Enter a Number to be factored: ")
+        if user_input.isdigit():
+            return int(user_input)
+        else:
+            print("Thats not a number bro , try again")
+          
 
-factorial = 1
+upper_bound = user_number()
+fact = factorial(upper_bound)
 
-print(f"The result of the factorial based on the given bound is {factorial}")
+print(f"The result of the factorial based on the given bound is {fact}")
 
-print("*"*75)
+print("-"*75)
 # Create a while loop that prompts a user for input of an integer values
 # Sum all inputs. When the user enters 'exit' (regardless of casing) end the loop
 # Upon ending the loop print the sum
@@ -36,12 +52,21 @@ print("*"*75)
     # I recommend checking out: https://www.w3schools.com/python/ref_string_replace.asp to figure out how one may remove a character from a string
 # All this together means you will have an intensive while loop that includes multiple if statements, likely with some nesting 
 # The sum should start at 0 
+num_sum = 0
+while True:
+    user_input = input("Enter a number to start adding: (Exit to stop) ")
+    if user_input.lower() == "exit":
+        break
 
-num_sum = 0 
+    if user_input.isdigit() or (user_input.startswith('-') and user_input[1:].isdigit()):
+        Number = int(user_input)
+        num_sum += Number
+    else:
+        print("Yo thats not a number, try again")
 
-print(f"Your final sum is {num_sum}")
-
-print("*"*75)
+print(f"The final sum is {num_sum}")
+    
+print("_"*75)
 # Now you will be creating a two operand calculator
 # It will support the following operators: +,-,/,*,% 
 # So accepted input is of the form `operand operator operand` 
@@ -58,5 +83,54 @@ print("*"*75)
     # So, it should function the same for `5 + 6` as `5+6`
 # Print the result of the equation
 # Again, loop through prompting the user for input until `exit` in any casing is input 
+def calculate(expression):
+    operators = ["+" , "-" , "/" , "%" , "*"]
+    operator = None
 
+    for char in expression:
+        if char in operators:
+            operator = char
+            break
+        
+    if operator is None:
+        print("This wont work and you know it, try again.")
+        return None
+        
+    operands = expression.split(operator)
+        
+    operands = [operand.strip() for operand in operands]
+
+    if len(operands) != 2:
+        print("This is invalid")
+        return None
+
+    if not (operands[0].isdigit() and operands[1].isdigit()):
+        print("Operands must be positive yo")
+        return None
+    
+    operand1 = int(operands[0])
+    operand2 = int(operands[1])
+
+    if operator == "+":
+        return operand1 + operand2 
+    elif operator == "-":
+        return operand1 - operand2 
+    elif operator == "*":
+        return operand1 * operand2 
+    elif operator == "/":
+        if operand2 == 0:
+            print("cant divide by 0 homie")
+            return None
+        return operand1 / operand2
+    elif operator == "%":
+        return operand1 % operand2
+
+while True:
+    user_input = input("Enter an expression:  (or Leave to quit) ")
+    if user_input.lower() == "leave":
+        break
+    final = calculate(user_input)
+    
+    if final is not None:
+        print(f"The answer to your equation is {final}")
         
